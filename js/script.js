8a7b3b6ff88883f4fbcc7b7da7fa71c4934dd883
Self_C <not_forgiving@list.ru>
2020-10-02 15:53:21 +0300

    Done second sector

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
     $('.copleted-slider').slick({
-        prevArrow: '<button type="button" class="arrow_prev"><img src="../img/arrow_left.svg"></button>',
-        nextArrow: '<button type="button" class="arrow_next"><img src="../img/arrow_right.svg"></button>'
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 1440,
+              settings: {
+                dots: false,
+                prevArrow: '<button type="button" class="arrow_prev"><img src="../img/arrow_left.svg"></button>',
+                nextArrow: '<button type="button" class="arrow_next"><img src="../img/arrow_right.svg"></button>'
+              }
+            },
+            {
+              breakpoint: 375,
+              settings: {
+                dots: true,
+                prevArrow: '<button type="button" class="arrow_prev"><img src="../img/arrow_left.svg"></button>',
+                nextArrow: '<button type="button" class="arrow_next"><img src="../img/arrow_right.svg"></button>'
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
     });
   });
